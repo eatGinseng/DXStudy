fa326947aee23036fb2c16a5db1de3698c8029c5
hongsam <eat.Ginseng@gmail.com>
2023-07-23 14:46:09 +0900

    shadow mapping 진행중

---
+++ RasterTek/ShadowMapping/LightClass.cpp
@@ -0,0 +1,71 @@
+#include "lightclass.h"
+
+
+LightClass::LightClass()
+{
+}
+
+
+LightClass::LightClass(const LightClass& other)
+{
+}
+
+
+LightClass::~LightClass()
+{
+}
+
+void LightClass::SetAmbientColor(float red, float green, float blue, float alpha)
+{
+	m_ambientColor = XMVectorSet(red, green, blue, alpha);
+	return;
+}
+
+void LightClass::SetDiffuseColor(float red, float green, float blue, float alpha)
+{
+	m_diffuseColor = XMVectorSet(red, green, blue, alpha);
+	return;
+}
+
+// light가 바라보는 방향, 이후에 light의 view matrix를 만드는 데 사용된다.
+void LightClass::SetLookAt(float x, float y, float z)
+{
+	m_lookAt.x = x;
+	m_lookAt.y = y;
+	m_lookAt.z = z;
+	return;
+
+}
+
+XMVECTOR LightClass::GetAmbientColor()
+{
+	return m_ambientColor;
+}
+
+XMVECTOR LightClass::GetDiffuseColor()
+{
+	return m_diffuseColor;
+}
+
+
+XMFLOAT3 LightClass::GetPosition()
+{
+	return m_position;
+}
+
+// light의 view matrix setup
+void LightClass::GenerateViewMatrix()
+{
+	XMFLOAT3 up;
+
+
+	// Setup the vector that points upwards.
+	up.x = 0.0f;
+	up.y = 1.0f;
+	up.z = 0.0f;
+
+	// Create the view matrix from the three vectors.
+	m_viewMatrix = XMMatrixLookAtLH(m_position, m_lookAt, up);
+
+	return;
+}
